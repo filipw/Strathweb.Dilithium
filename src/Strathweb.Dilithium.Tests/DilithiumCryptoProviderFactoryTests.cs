d05cdd02a22f3bc06f289a2d170cbb853333008e
filipw <wojcieszyn@gmail.com>
2023-07-29 13:25:11 +0200

    two more tests

--- src/Strathweb.Dilithium.Tests/DilithiumCryptoProviderFactoryTests.cs
+++ src/Strathweb.Dilithium.Tests/DilithiumCryptoProviderFactoryTests.cs
@@ -20,6 +20,24 @@ public class DilithiumCryptoProviderFactoryTests
         var factory = new DilithiumCryptoProviderFactory();
         Assert.Throws<NotSupportedException>(() => factory.CreateForVerifying(testKey, "CRYDI3"));
     }
+    
+    [Fact]
+    public void CreateForSigning_ReturnsDilithiumSignatureProvider()
+    {
+        var testKey = new DilithiumSecurityKey("CRYDI3");
+        var factory = new DilithiumCryptoProviderFactory();
+        var signatureProvider = factory.CreateForSigning(testKey, "CRYDI3");
+        Assert.IsType<DilithiumSignatureProvider>(signatureProvider);
+    }
+    
+    [Fact]
+    public void CreateForVerifying_ReturnsDilithiumSignatureProvider()
+    {
+        var testKey = new DilithiumSecurityKey("CRYDI3");
+        var factory = new DilithiumCryptoProviderFactory();
+        var signatureProvider = factory.CreateForVerifying(testKey, "CRYDI3");
+        Assert.IsType<DilithiumSignatureProvider>(signatureProvider);
+    }
 }
 
 class TestSecurityKey : SecurityKey
