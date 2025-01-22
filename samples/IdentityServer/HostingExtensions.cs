a53d87d4e93baebba374460f08504b3bbdb5e4d4
filipw <filip@strathweb.com>
2025-01-22 20:36:00 +0100

    replaced crydi sample with ml-dsa-44

--- samples/IdentityServer/HostingExtensions.cs
+++ samples/IdentityServer/HostingExtensions.cs
@@ -4,6 +4,7 @@ using Duende.IdentityServer.ResponseHandling;
 using Duende.IdentityServer.Services.KeyManagement;
 using Strathweb.Dilithium.DuendeIdentityServer;
 using Strathweb.Dilithium.DuendeIdentityServer.KeyManagement;
+using Strathweb.Dilithium.IdentityModel;
 using JsonWebKey = Microsoft.IdentityModel.Tokens.JsonWebKey;
 
 namespace IdentityServer;
@@ -12,7 +13,7 @@ internal static class HostingExtensions
 {
     public static WebApplication ConfigureServices(this WebApplicationBuilder builder)
     {
-        //var rawJwk = File.ReadAllText(Path.Combine(Directory.GetCurrentDirectory(), "crydi3.json"));
+        //var rawJwk = File.ReadAllText(Path.Combine(Directory.GetCurrentDirectory(), "ml-dsa-44.json"));
         //var jwk = JsonSerializer.Deserialize<JsonWebKey>(rawJwk);
 
         builder.Services.AddIdentityServer(opt =>
